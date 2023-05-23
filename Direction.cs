1f68920850d4508094bf32316248c22291e7a64e
Sujin Lee <sujinbonnaud@gmail.com>
2023-05-23 12:17:24 -0700

    used ctr+1, Generate Equals and GetHashCode

--- Direction.cs
+++ Direction.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SnakeGameSJ
 {
-    class Direction
+    public class Direction
     {
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+        public int RowOffset { get; }
+        public int ColumnOffset { get; }
+
+        private Direction(int Rowoffset, int Columnoffset)
+        {
+            RowOffset = Rowoffset;
+            ColumnOffset = Columnoffset;
+        }
+
+        public Direction Opposite()
+        {
+            return new Direction(-RowOffset, -ColumnOffset);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Direction direction &&
+                   RowOffset == direction.RowOffset &&
+                   ColumnOffset == direction.ColumnOffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColumnOffset);
+        }
+
+        public static bool operator ==(Direction? left, Direction? right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction? left, Direction? right)
+        {
+            return !(left == right);
+        }
     }
 }
