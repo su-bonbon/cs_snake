19468a9f06b6ebb0687670b6c39f923aa9c90d95
Sujin Lee <sujinbonnaud@gmail.com>
2023-05-23 12:23:34 -0700

    Used ctr+., Generate Equal and GetHashCode + Operator

--- Position.cs
+++ Position.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SnakeGameSJ
 {
-    class Position
+    public class Position
     {
+        public int Row { get; }
+        public int Col { get; }
+
+        public Position(int row, int col)
+        {
+            Row = row; Col = col;
+        }
+
+        public Position Translate(Direction dir)
+        {
+            return new Position(Row + dir.RowOffset, Col + dir.ColumnOffset);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Position position &&
+                   Row == position.Row &&
+                   Col == position.Col;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Col);
+        }
+
+        public static bool operator ==(Position? left, Position? right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position? left, Position? right)
+        {
+            return !(left == right);
+        }
     }
 }
