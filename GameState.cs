9f0d194c2162811dcecbd0458a8181a402b4aba0
Sujin Lee <sujinbonnaud@gmail.com>
2023-05-23 12:37:03 -0700

    create methods in gamestatus class

--- GameState.cs
+++ GameState.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace SnakeGameSJ
 {
-    class GameState
+    public class GameState
     {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows, cols];
+            Dir = Direction.Right;
+
+            AddSnake();
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+
+            for (int c = 1; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(r, c));
+            }
+
+        }
+
+        private IEnumerable<Position> EmptyPosition()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c <= Cols; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+        
     }
 }
