9a6d98871e321d63e6d4d1ad0407b3bd68ae6dbc
Sujin Lee <sujinbonnaud@gmail.com>
2023-05-28 19:16:57 -0700

    call Image constructor in main frame

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,34 @@ namespace SnakeGameSJ
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int rows = 15, cols = 15;
+        private readonly Image[,] gridImages;
+
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = SetupGrid();
+        }
+        private Image[,] SetupGrid()
+        {
+            Image[,] images= new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+
+            for (int r = 0; r < cols; r++)
+            {
+                for (int c = 0; c < cols; ++c)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+            return images;
         }
     }
 }
